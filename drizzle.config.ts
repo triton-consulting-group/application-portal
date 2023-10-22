48ffadba47b30816340c62e65d9783217a3395bc
panda2k <thelowkeypanda@gmail.com>
2023-10-22 11:20:53 -0700

    base scaffolding with google auth

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env.mjs";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  driver: "mysql2",
+  dbCredentials: {
+    connectionString: env.DATABASE_URL,
+  },
+  tablesFilter: ["tcg-application-portal_*"],
+} satisfies Config;
