51b207f89cfb5424221a91f7bdcb33a97a922355
panda2k <thelowkeypanda@gmail.com>
2024-01-11 22:11:17 -0800

    rearrange home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,54 +6,82 @@ import { api } from "~/trpc/server";
 import { Instagram } from "lucide-react";
 import Image from "next/image";
 import { Button } from "~/components/ui/button";
+import { Application, RecruitmentCycle } from "./types";
 
-export default async function Home() {
-    const session = await getServerAuthSession();
+function RecruitmentCycleText({
+    activeCycle,
+}: {
+    activeCycle: RecruitmentCycle | undefined
+}) {
+    const formatDate = (d: Date): string => {
+        return `${d.toLocaleDateString('en-us', { weekday: "long", month: "short", day: "numeric" })} ${d.toLocaleTimeString()}`;
+    };
 
-    async function ActionButton({ role }: { role: Role | undefined }) {
-        if (role === Role.APPLICANT) {
-            const activeCycle = await api.recruitmentCycle.getActive.query();
-            if (!activeCycle) {
-                return (
-                    <div className="flex flex-col items-center gap-y-2">
-                        <h2 className="text-center">
-                            We aren't currently accepting applications. <br />
-                            Follow us on Instagram to be notified of when our Fall and Winter recruitment begins!
-                        </h2>
-                        <Button>
-                            <Link href="https://www.instagram.com/ucsdtcg/">
-                                <Instagram />
-                                @ucsdtcg
-                            </Link>
-                        </Button>
-                    </div>
-                );
-            }
+    if (activeCycle) {
+        return (
+            <h2 className="text-center text-lg">
+                We are currently recruiting for {activeCycle.displayName}! <br />
+                Applications are due {formatDate(activeCycle.endTime)}
+            </h2>
+        );
+    } else {
+        return (
+            <h2 className="text-center text-lg">
+                We aren't currently accepting applications. <br />
+                Follow us on Instagram to be notified of when our Fall and Winter recruitment begins!
+            </h2>
+        );
+    }
+}
 
-            const application = await api.application.getUserApplicationByCycleId.query(activeCycle.id);
-
+function ActionButton({
+    role,
+    application,
+    activeCycle
+}: {
+    role: Role | undefined,
+    application: Application | undefined,
+    activeCycle: RecruitmentCycle | undefined
+}) {
+    if (role === Role.APPLICANT) {
+        if (!activeCycle) {
             return (
                 <Button>
-                    <Link href="/apply">
-                        {!application
-                            ? "Start your application"
-                            : application.submitted
-                                ? "View your submitted application"
-                                : "Continue your application"
-                        }
-                    </Link>
-                </Button>
-            );
-        } else if (role === Role.ADMIN || role === Role.MEMBER) {
-            return (
-                <Button>
-                    <Link href="/dashboard">
-                        Dashboard
+                    <Link href="https://www.instagram.com/ucsdtcg/" className="flex items-center gap-x-2">
+                        <Instagram />
+                        @ucsdtcg
                     </Link>
                 </Button>
             );
         }
+
+        return (
+            <Button>
+                <Link href="/apply">
+                    {!application
+                        ? "Start your application"
+                        : application.submitted
+                            ? "View your submitted application"
+                            : "Continue your application"
+                    }
+                </Link>
+            </Button>
+        );
+    } else if (role === Role.ADMIN || role === Role.MEMBER) {
+        return (
+            <Button>
+                <Link href="/dashboard">
+                    Dashboard
+                </Link>
+            </Button>
+        );
     }
+}
+
+export default async function Home() {
+    const session = await getServerAuthSession();
+    const activeCycle = await api.recruitmentCycle.getActive.query();
+    const application = activeCycle ? await api.application.getUserApplicationByCycleId.query(activeCycle.id) : undefined;
 
     return (
         <div className="min-h-screen flex flex-col">
@@ -61,18 +89,27 @@ export default async function Home() {
                 <Image alt="TCG Logo" height={48} width={136} src="/logo.png" />
             </div>
             <main className="flex flex-col grow items-center justify-center text-primary-background">
-                <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-                    <h1 className="text-5xl text-center font-extrabold tracking-tight sm:text-[5rem]">
-                        Applicant Portal
-                    </h1>
-                    <div className="flex flex-col items-center gap-4">
-                        <div className="flex flex-col items-center justify-center gap-4">
-                            <p className="text-center text-2xl">
-                                {session && <span>Welcome back {session.user?.name?.split(" ")[0]}</span>}
-                            </p>
+                <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16">
+                    <div className="flex flex-col gap-y-4">
+                        <h1 className="text-5xl text-center font-extrabold tracking-tight sm:text-[5rem]">
+                            Applicant Portal
+                        </h1>
+                    </div>
+                    <div className="flex flex-col items-center gap-y-3">
+                        <p className="text-center text-2xl">
+                            {session && <span>Welcome back {session.user?.name?.split(" ")[0]}</span>}
+                        </p>
+                        {session?.user.role === Role.APPLICANT &&
+                            <RecruitmentCycleText activeCycle={activeCycle} />
+                        }
+                        <div className="flex gap-x-3">
+                            <ActionButton
+                                activeCycle={activeCycle}
+                                application={application}
+                                role={session?.user.role}
+                            ></ActionButton>
+                            <AuthButton loggedIn={session !== null}></AuthButton>
                         </div>
-                        <ActionButton role={session?.user.role}></ActionButton>
-                        <AuthButton loggedIn={session !== null}></AuthButton>
                     </div>
                 </div>
             </main>
