b62c3fcd615634a87c0315aa5c86cf8a2896f3d4
panda2k <thelowkeypanda@gmail.com>
2024-01-23 22:12:29 -0800

    remove unused procedure

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -76,29 +76,6 @@ export const createTRPCRouter = t.router;
  */
 export const publicProcedure = t.procedure;
 
-/** Reusable middleware that enforces users are logged in before running the procedure. */
-const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-    if (!ctx.session || !ctx.session.user) {
-        throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-    return next({
-        ctx: {
-            // infers the `session` as non-nullable
-            session: { ...ctx.session, user: ctx.session.user },
-        },
-    });
-});
-
-/**
- * Authenticated procedure
- *
- * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
- * the session is valid and guarantees `ctx.session.user` is not null.
- *
- * @see https://trpc.io/docs/procedures
- */
-export const authenticatedProcedure = t.procedure.use(enforceUserIsAuthed);
-
 /** Resuable middleware that enforces users are applicants before running the procedure */
 const enforceUserIsApplicant = t.middleware(({ ctx, next }) => {
     if (!ctx.session || !ctx.session.user) {
